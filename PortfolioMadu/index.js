b852d18bfa4a4e12a1226f9015ccf23e9f058c4c
Maria Eduarda Castro <maduucastro2007@icloud.com>
2024-06-05 16:05:36 -0300

    atualização

--- PortfolioMadu/index.js
+++ PortfolioMadu/index.js
@@ -30,9 +30,20 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
   })
 }
 
-// ---
+//---
 const headerLogoConatiner = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+function sendMail(){
+  var params = {
+    from_name : document.getElementById("name").value,
+    from_email : document.getElementById("email").value,
+    message : document.getElementById("message").value,
+  }
+
+  emailjs.send("service_odt9bg5","template_ggrwvgk",params)
+  .then(alert("Sua mensagem foi enviada com sucesso!!"));
+}
